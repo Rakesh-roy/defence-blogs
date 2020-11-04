564b5d3e840cf5855ff23a356bd4d3626983abf4
Rakesh-roy <rakeshroy20031997@gmail.com>
2020-11-04 19:19:06 +0530

    Add files via upload

---
+++ blogs/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+from django.views.generic import TemplateView
+from Defence_Blogs import settings
+from blogs import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('Blogs/', views.blogs, name='blogs'),
+    path('All_Blogs/', views.allBlogs, name='all_blogs'),
+    path('Post/', views.post, name='post'),
+    path('Contact/', TemplateView.as_view(template_name='contact.html'), name='contact'),
+    path('Feedback/', views.get_feedback, name='get_feedback')
+
+]
+
+
+if settings.DEBUG:
+    urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
