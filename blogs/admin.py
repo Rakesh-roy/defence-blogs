9597495cbaaf8411498134c5de3011c819b607ac
Rakesh-roy <rakeshroy20031997@gmail.com>
2020-11-05 20:52:20 +0530

    Add files via upload

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blogs.models import BlogsModel
+from blogs.models import BlogsModel,Customers
 
 
 # Register your models here.
@@ -7,5 +7,10 @@ from blogs.models import BlogsModel
 @admin.register(BlogsModel)
 class BlogsAdmin(admin.ModelAdmin):
     list_display = ('id','blog_title','type','intro','author','published_time')
+
+
+@admin.register(Customers)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'querry','message')
 
 
